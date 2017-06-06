9d759cddde5190c691ab546fec42322a7cd47163
Chao <362228416@qq.com>
2017-06-06 11:52:25 +0800

    Update install-openresty.sh
    add sudo

--- install/install-openresty.sh
+++ install/install-openresty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install -y libpcre3 libpcre3-dev
-apt-get install -y openssl libssl-dev
+sudo apt-get update
+sudo apt-get install -y libpcre3 libpcre3-dev
+sudo apt-get install -y openssl libssl-dev
 cd /tmp
 version="1.11.2.2"
 name=openresty-$version
@@ -13,5 +13,5 @@ tar zxvf $file
 cd $name
 ./configure
 make -j2
-make install
-ln -s $prefix/nginx/sbin/nginx /usr/local/sbin/openresty
+sudo make install
+sudo ln -s $prefix/nginx/sbin/nginx /usr/local/sbin/openresty
